abc884b034176264e090b3ca6f2497249798e222
pierrelyuvc80y <pierrelyuvc80y@gmail.com>
2026-05-29 09:48:18 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = '123vietnamese-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/lessons-data.json',
+  '/manifest.json',
+  '/icon-192.jpg',
+  '/icon-512.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
